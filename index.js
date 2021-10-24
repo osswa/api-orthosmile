25b05f258711004a63f559a8a5955cc922824dfb
Oswaldo Granados <ogranados@ts4.mx>
2021-10-24 17:58:29 -0500

    se cambia base de datos mongo

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var app = require('./app');
 var port = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/orthosmiledb', (err, res) => {
+//mongoose.connect('mongodb://localhost:27017/orthosmiledb', (err, res) => {
+mongoose.connect('mongodb+srv://granpas:granpas@cluster0.blcmi.mongodb.net/orthosmiledb', (err, res) => {
 	if(err) {
 		throw err;
 	} else {
